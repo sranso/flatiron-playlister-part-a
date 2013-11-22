4e3a3e4792df53e7a85563b93bd4d171f4112dea
Blake Johnson <blake41@gmail.com>
2013-11-22 15:17:37 -0500

    small fixes

--- playlister_directions.rb
+++ playlister_directions.rb
@@ -5,6 +5,6 @@
 # These files should be placed within a lib directory and required on the top of
 # any script that utilizes them (including this test script). Once required
 # all the tests within this suite should pass.
-# Before you start trying to make tests pass, think through artists, songs and genres are related in the real world.
+# Before you start trying to make tests pass, think through how artists, songs and genres are related in the real world.
 # Our goal is to model the real world using classes and relationships between objects.  Any relationship you
 # create should intuitively make sense to you.  
